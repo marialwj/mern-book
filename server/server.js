7bb746b2f1faf3787837b7332a71b3f6610d90e6
marialwj <mariaa.georgy@gmail.com>
2024-04-07 14:42:25 -0700

    auth.js changes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
